6ff4050950db5ae230ac68b86f641c38964bc047
potatogallery <khazsaunders@hotmail.com>
2021-10-01 20:06:05 +0200

    Files

---
+++ ADPFinalproj/src/main/java/za/ac/cput/adpfinalproj/clientgui/CGui.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.adpfinalproj.clientgui;
+
+/**
+ *
+ * @author POTATOE
+ */
+public class CGui {
+    
+}
