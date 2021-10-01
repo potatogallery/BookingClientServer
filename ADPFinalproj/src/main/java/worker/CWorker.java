a552d015ab3a958c73d27685fa177500eeb94020
Raeece <raeecesamuels100@gmail.com>
2021-10-01 21:22:08 +0200

    Update CWorker.java

--- ADPFinalproj/src/main/java/worker/CWorker.java
+++ ADPFinalproj/src/main/java/worker/CWorker.java
@@ -11,4 +11,33 @@ package worker;
  */
 public class CWorker {
     
+   private String fname, lname;
+
+    public CWorker(String fname, String lname) {
+        this.fname = fname;
+        this.lname = lname;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    @Override
+    public String toString() {
+        return "CWorker{" + "fname=" + fname + ", lname=" + lname + '}';
+    }
+   
+   
 }
