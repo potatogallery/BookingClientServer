1b19f79f09170b8e7a3ff9126b37e2893c17096e
Pain600 <81654846+Pain600@users.noreply.github.com>
2021-10-15 01:54:57 +0200

    Add files via upload

---
+++ ADPFinalProjServer/src/main/java/za/ac/cput/adpfinalprojserver/main/ServerMain.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.adpfinalprojserver.main;
+
+/**
+ *
+ * @author raeec
+ */
+public class ServerMain {
+    public static void main(String[] args) {
+        new Server();
+    }
+}
